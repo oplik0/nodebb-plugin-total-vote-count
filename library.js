7238a1fdbfa44600ee01bf0c7633d01494b0ce05
Opliko <opliko.reg@protonmail.com>
2023-08-05 22:15:37 +0000

    fix: don't double count votes

--- library.js
+++ library.js
@@ -13,8 +13,6 @@ plugin.getTopics = async (hookData) => {
 			for (const { score } of voteData[index]) {
 				topic.votes += score;
 			}
-			const allVotes = voteData[index].reduce((all, current) => all + current.score, 0);
-			topic.votes += allVotes;
 		}
 	}
 	return hookData;
