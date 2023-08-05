d93e30abccedf9a0f84a18d945025d5acb086235
Opliko <opliko.reg@protonmail.com>
2023-08-05 22:57:24 +0000

    feat: error handling

--- library.js
+++ library.js
@@ -1,13 +1,21 @@
 'use strict';
 
+const winston = require.main.require('winston');
+
 const db = require.main.require('./src/database');
 
 const plugin = {};
 
 plugin.getTopics = async (hookData) => {
-	const voteData = await db.getSortedSetsMembersWithScores(
-		hookData.topics.map(t => `tid:${t.tid}:posts:votes`)
-	);
+	let voteData;
+	try {
+		voteData = await db.getSortedSetsMembersWithScores(
+			hookData.topics.map(t => `tid:${t.tid}:posts:votes`)
+		);
+	} catch (e) {
+		winston.error(`Error getting vote data ${e}`);
+		return hookData;
+	}
 	for (const [index, topic] of Object.entries(hookData.topics)) {
 		if (topic) {
 			for (const { score } of voteData[index]) {
