5553627685b7bc708f06e490157a39a3db9f19c1
Barış Soner Uşaklı <barisusakli@gmail.com>
2024-03-20 12:02:24 -0400

    feat: on vote calculate new order (#19)
    * feat: on vote calculate new order
    
    * fix: resolve eslint issues
    
    ---------
    
    Co-authored-by: Opliko <opliko.reg@protonmail.com>

--- library.js
+++ library.js
@@ -3,8 +3,9 @@
 const winston = require.main.require('winston');
 
 const db = require.main.require('./src/database');
+const topics = require.main.require('./src/topics');
 
-const plugin = {};
+const plugin = module.exports;
 
 plugin.getTopics = async (hookData) => {
 	let voteData;
@@ -26,4 +27,22 @@ plugin.getTopics = async (hookData) => {
 	return hookData;
 };
 
-module.exports = plugin;
+plugin.updatePostVoteCount = async (hookData) => {
+	const { tid } = hookData.post;
+	const topicData = await topics.getTopicFields(tid, ['cid', 'upvotes', 'downvotes', 'pinned']);
+	const voteData = await db.getSortedSetMembersWithScores(`tid:${tid}:posts:votes`);
+
+	let { votes } = topicData;
+	for (const { score } of voteData) {
+		votes += score;
+	}
+
+	const promises = [
+		db.sortedSetAdd('topics:votes', votes, tid),
+	];
+
+	if (!topicData.pinned) {
+		promises.push(db.sortedSetAdd(`cid:${topicData.cid}:tids:votes`, votes, tid));
+	}
+	await Promise.all(promises);
+};
